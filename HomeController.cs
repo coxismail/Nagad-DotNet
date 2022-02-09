a389a64612095669bdd1926c44935c076f476001
Mohammad Ismail <49612845+coxismail@users.noreply.github.com>
2022-02-09 07:56:23 +0600

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebApplication1testnagad.Models;
+using Nagad.Models;
+namespace WebApplication1testnagad.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+           
+            var Nagadhelper = new NagadHelperClass();
+            try
+            {
+                var p = await Nagadhelper.client.Payment.Initialize("6534");
+                if (p.PaymentReferenceId != null && p.PaymentReferenceId != string.Empty)
+                {
+                    decimal amount = Convert.ToDecimal(100.00);
+                    var c = new CompletePaymentRequest("050", amount, p.Random, "6534");
+                  var comp =  await Nagadhelper.client.Payment.Complete(p.PaymentReferenceId, c);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Message = ex.Message;
+            }
+            //var p = await myclass.client.Payment.Complete("6534");
+
+            return View();
+        }
+
+
+    }
+}
---
+++ NagadHelperClass.cs
@@ -0,0 +1,30 @@
+﻿using Nagad;
+
+namespace WebApplication1testnagad.Models
+{
+    public class NagadHelperClass
+    {
+        public NagadClient client { get; set; }
+
+        public NagadHelperClass()
+        {
+
+            client = new NagadClient(new Credentials(NagadInfo.marchendId, NagadInfo.publickey, NagadInfo.privatekey, null, NagadInfo.AcNo), BaseMode.SandBox); client.TimeZone = System.TimeZoneInfo.FindSystemTimeZoneById("Bangladesh Standard Time");
+            client.APIVersion = "v-0.2.0";
+        }
+    }
+    public static class NagadInfo
+    {
+        public static string marchendId { get; set; }
+        public static string publickey { get; set; }
+        public static string privatekey { get; set; }
+        public static string AcNo { get; set; }
+        static NagadInfo()
+        {
+            AcNo = " ";
+            marchendId = " ";
+            publickey = " ";
+            privatekey = " ";
+        }
+    }
+}
